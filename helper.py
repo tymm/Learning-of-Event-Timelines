404efb99a01213807accbdc0bc2b90f36024ea35
Timm <tymmm1@gmail.com>
2014-02-08 17:35:03 +0100

    Added functions to get the events surrounding text

--- helper.py
+++ helper.py
@@ -20,3 +20,56 @@ def get_wordnet_similarity(word1, word2):
         return maxSim
     else:
         return 0
+
+# Returns the surrounding text of an event
+def get_surrounding(textfile, dirname, event_begin, event_end, words_left, words_right):
+    with open(dirname+"/"+textfile, "r") as f:
+        f.seek(event_begin)
+        left = get_surrounding_words(f, "left", words_left)
+
+def get_surrounding_words(file, direction, n_words):
+    if direction == "left":
+        steps = 0
+        going_left = True
+
+        for i in range(n_words+1):
+            # Go left until we hit a space, newline or the beginning of the textfile
+            going_left = True
+            while going_left:
+                # Can't go further
+                if file.tell() == 0:
+                    going_left = False
+                    break
+
+                # Go one left
+                file.seek(file.tell()-1)
+                steps += 1
+
+                # Go to next word
+                c = file.read(1)
+                if c == ' ' or c == '\n':
+                    going_left = False
+                # file.read(1) moves forward, so we have to go back
+                file.seek(file.tell()-1)
+
+        return file.read(steps).strip()
+
+    if direction == "right":
+        steps = 0
+        going_right = True
+        text = ""
+
+        for i in range(n_words+1):
+            # Go right until we hit a space or a newline
+            going_right = True
+            while going_right:
+                c = file.read(1)
+                if c == ' ' or c == '\n' or c == '':
+                    going_right = False
+
+                text = text + c
+            print "word"
+
+        return text
+
+
