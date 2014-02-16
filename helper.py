f4ddf9892f65131d7efff6c4b71233d8a9c71506
Timm <tymmm1@gmail.com>
2014-02-16 01:51:14 +0100

    Successful extraction of surrounding text of events

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
 from nltk.corpus import wordnet as wn
+import string
 import numpy
+from cStringIO import StringIO
+import re
 
 # Get the similarity of two words
 # We get it by taking the maximal similarity we can get when looking at both synsets
@@ -22,59 +25,104 @@ def get_wordnet_similarity(word1, word2):
     else:
         return 0
 
-# Returns the surrounding text of an event (event included in text)
-def get_surrounding(event_text, textfile, dirname, event_begin, event_end, words_left, words_right):
+# Preprocessing of sentence
+def preprocess_sentence(text):
+        def repl(m):
+            return m.group(1) + " " + m.group(2)
+        # Removing newlines
+        text_tmp = re.sub(r"(\w+)\n(\w+)", repl, text)
+        text_tmp = re.sub(r"$\n", "", text_tmp)
+
+        # Removing everything between two words and replacing it by a space
+        text_tmp = re.sub(r"(\w+)\W*[,\-\"']+\W*(\w+)", repl, text_tmp)
+        # Run again because re.sub only matches non overlapping stuff. "you, pray," -> "you pray,"
+        text_tmp = re.sub(r"(\w+)\W*[,\-\"']+\W*(\w+)", repl, text_tmp)
+
+        # Removing sentences endings (?!.)
+        text_tmp = re.sub(r"[?!.]", "", text_tmp)
+
+        return text_tmp
+
+
+# Returns the sentence the event is in
+def get_sentence(event_text, textfile, dirname, event_begin):
     with open(dirname+"/"+textfile, "r") as f:
+        # Read the text left of the event
         f.seek(event_begin)
-        left = get_surrounding_words(f, "left", words_left)
-        f.seek(event_end)
-        right = get_surrounding_words(f, "right", words_right)
-        return left + " " + event_text + " " + right
+        begin = go_left_until_point(f)
 
-def get_surrounding_words(file, direction, n_words):
-    if direction == "left":
-        steps = 0
-        going_left = True
+        # Read the text right of the event
+        f.seek(event_begin)
+        end = go_right_until_point(f)
 
-        for i in range(n_words+1):
-            # Go left until we hit a space, newline or the beginning of the textfile
-            going_left = True
-            while going_left:
-                # Can't go further
-                if file.tell() == 0:
-                    going_left = False
-                    break
+        # Look how many words with the same text of the event are in front of the event we are interested in
+        same_words_before_event = count_words(f, event_text, event_begin, begin)
 
-                # Go one left
-                file.seek(file.tell()-1)
-                steps += 1
+        # Return the sentence
+        f.seek(begin)
+        return (f.read(end-begin).strip(".").strip(), same_words_before_event)
 
-                # Go to next word
-                c = file.read(1)
-                if c == ' ' or c == '\n':
-                    going_left = False
-                # file.read(1) moves forward, so we have to go back
-                file.seek(file.tell()-1)
+def count_words(f, event_text, event_begin, text_begin):
+    f.seek(text_begin)
 
-        return file.read(steps).strip()
+    tmp = ""
+    count = 0
+    while f.tell() < event_begin:
+        c = f.read(1)
+        if c == event_text[0]:
+            tmp = c + f.read(len(event_text)-1)
+            if tmp == event_text:
+                # Case: fell - felling
+                c = f.read(1)
+                if c == " " or c == "-":
+                    count += 1
+            tmp = ""
 
-    if direction == "right":
-        steps = 0
-        going_right = True
-        text = ""
+    return count
 
-        for i in range(n_words+1):
-            # Go right until we hit a space or a newline
-            going_right = True
-            while going_right:
-                c = file.read(1)
-                if c == ' ' or c == '\n' or c == '':
-                    going_right = False
+# Returns the surrounding text of an event (event included in text)
+def get_surrounding(event_text, textfile, dirname, event_begin, words_left, words_right):
+    sentence, same_words_before_event = get_sentence(event_text, textfile, dirname, event_begin)
 
-                text = text + c
+    # Remove everything which is not a word
+    sentence = preprocess_sentence(sentence)
 
-        return text.strip()
+    # Turn sentence into a list of words
+    words = sentence.split()
 
+    # Getting the index of the event
+    index = None
+    count = 0
+    for i, word in enumerate(words):
+        if word == event_text:
+            if count == same_words_before_event:
+                index = i
+                break
+            count += 1
+
+    # Returning the surrounding area
+    if (index-words_left) >= 0:
+        return string.join(words[(index-words_left):(index+words_right+1)], " ")
+    else:
+        return string.join(words[0:(index+words_right+1)], " ")
+
+def go_left_until_point(f):
+    c = f.read(1)
+    f.seek(f.tell()-1)
+    while c != "." and c != "?" and c != "!" and f.tell() != 0:
+        # Go back one character
+        f.seek(f.tell()-1)
+        c = f.read(1)
+        f.seek(f.tell()-1)
+
+    return f.tell()
+
+def go_right_until_point(f):
+    c = f.read(1)
+    while c != "." and c != "?" and c != "!" and c != "":
+        c = f.read(1)
+
+    return f.tell()
 
 def get_stem_class(stems, stem):
     return numpy.where(stems==stem)[0][0]
