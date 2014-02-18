dc9b075b86b7ba2588b42f99452f93ae7390a016
Timm <tymmm1@gmail.com>
2014-02-18 15:31:56 +0100

    Refactoring and running on all features

--- train.py
+++ train.py
@@ -3,14 +3,7 @@ import pandas as pd
 import numpy as np
 from extract_features import Feature
 from parser import parseXML
-import pandas as pd
 from helper import get_stem_class
-
-def split(indices):
-    """ split indices into two parts: 80%, 20% """
-    size = indices.shape[0]
-    x = size * 0.8
-    return indices[:x], indices[x:]
 
 # Create numpy array with samples and targets
 data = parseXML("fables-100-temporal-dependency.xml", "McIntyreLapata09Resources/fables/")
@@ -28,11 +21,12 @@ for txt in data.textfiles:
         stems = np.append(stems, [f.get_stem_source()])
 
 stems = np.unique(stems)
-print stems.shape
 
-X = np.array([], dtype=float).reshape(0,4)
+X = np.array([], dtype=float).reshape(0,5)
 y = np.array([], dtype=int)
 
+null = 0
+none = 0
 for txt in data.textfiles:
     # Use union relations
     txt.compute_union_relations()
@@ -41,24 +35,31 @@ for txt in data.textfiles:
         # If the time relation is not in (before, contains, is_contained_in), skip
         if f.get_category() == -1:
             continue
-        X = np.append(X, [[f.get_distance(), f.get_similarity_of_words(), get_stem_class(stems, f.get_stem_target()), get_stem_class(stems, f.get_stem_source())]], axis=0)
+        if f.get_category() == 0:
+            null += 1
+            """
+        if not f.get_aspect_combined():
+            none += 1
+            """
+        if null > 450:
+            continue
+        # Building a row of all feature values
+        X = np.append(X, [[f.get_distance(), f.get_similarity_of_words(), f.get_aspect_combined(), f.get_polarity(), f.get_modality()]], axis=0)
         y = np.append(y, [f.get_category()])
 
-print X.shape
-print y.shape
-np.set_printoptions(threshold=4000)
-print y
 
-df_samples = pd.DataFrame(X, columns=['distance between events', 'similarity between words', 'stem of target event', 'stem of source event'])
-
-# split dataset in training and test set
-len_train = len(df_samples)*80/100
-df_train, df_test = df_samples[:len_train], df_samples[len_train:]
+# Split dataset in training set(80%) and test set (20%)
+len_train = len(X)*80/100
+X_train, X_test = X[:len_train], X[len_train:]
 y_train, y_test = y[:len_train], y[len_train:]
 
-# train classifier
-rf = RandomForestClassifier(n_estimators=1000)
-rf.fit(df_train, y_train)
+# Train the random forest classifier
+rf = RandomForestClassifier(n_jobs=2, n_estimators=100)
+rf.fit(X_train, y_train)
 
-# print accuracy
-print rf.score(df_test, y_test)
+# Print accuracy
+print rf.score(X_test, y_test)
+print "Predicted:"
+print rf.predict(X_test)
+print "True values:"
+print y_test
