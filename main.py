f5b49a3207bcb4a11f751b5aef0f7122d00ec57b
Timm <tymmm1@gmail.com>
2014-04-08 23:02:59 +0200

    Tidied up

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from train import *
 
-def estimate(X_, y_, class_id):
-    X = list(X_)
-    y = list(y_)
+def get_prediction(temporal_rel, new=True, annotations="union", features=["pos", "stem", "aspect", "tense", "distance", "similarity", "polarity", "modality"]):
+    """This function is suited to test the different options and get a result for them
 
-    # Adjust y for classification
-    n = 0
-    for i, cat in enumerate(y):
-        if cat == class_id:
-            y[i] = 1
-            n += 1
-        else:
-            y[i] = 0
-    print n
+    Args:
+        temporal_rel (TemporalRelation): temporal relation we are interested in.
+        new (bool): Load features from file (False) or calculate them new (True)?
+        annotations (str): Either "union" or "intersected". All data vs. data which annotators have in common
+        features (list): Which features should be used
+
+    """
+    X, y = load_data(new, temporal_rel, annotations, features)
 
     # Split dataset into training set (80%) and test set (20%)
     X_train, X_test, y_train, y_test = split(X, y)
@@ -30,12 +28,11 @@ def estimate(X_, y_, class_id):
     print f1_score(y_test, rf.predict(X_test))
     print
 
-    if class_id == 3 or class_id == 2:
-        print "Ground truth"
-        print y_test
-        print
-        print "Predicted"
-        print rf.predict(X_test)
+    print "Ground truth"
+    print y_test
+    print
+    print "Predicted"
+    print rf.predict(X_test)
 
 if __name__ == "__main__":
     # With 'python main.py --reload' a "fresh parsing" will be enforced
@@ -44,29 +41,20 @@ if __name__ == "__main__":
     else:
         new = False
 
-    print "Loading"
-    # Load the data which is needed to train the classifier.
-    X, y = load_data(new, "union", features=["tense", "aspect"])
-    print "Done loading"
-
-    # Information on class 0
-    print "Information for class 2"
-    estimate(X, y, 2)
+    print "Information for class 0"
+    get_prediction(0)
+    print
 
     """
-    print
-    # Information on class 1
     print "Information for class 1"
-    estimate(X, y, 1)
-
+    get_prediction(1)
     print
-    # Information on class 2
+
     print "Information for class 2"
-    estimate(X, y, 2)
-
+    get_prediction(2)
     print
-    # Information on class 3
-    print "Information for class 3"
-    estimate(X, y, 3)
-    """
 
+    print "Information for class 3"
+    get_prediction(3)
+    print
+    """
